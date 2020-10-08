80ff237b97151b546bca797a8ef06c0cfb37571f
duynm_58 <duynm58@gmail.com>
2020-10-08 20:43:19 +0700

    set ads to false and change icon

--- public/javascripts/detail_ads_click.js
+++ public/javascripts/detail_ads_click.js
@@ -2,7 +2,7 @@ $(document).ready(function () {
   var count_on_click = 0;
 
   $(document).off("click", ".select-video-to-play").on("click", ".select-video-to-play", function() {
-    if (count_on_click % 2 == 1) {
+    if (is_need_ads === true && count_on_click % 2 == 1) {
       var win = window.open('//a.o333o.com/api/direct/278171?s1=&kw=', '_blank');
       if (win) {
           win.focus();
--- public/javascripts/search_ads_click.js
+++ public/javascripts/search_ads_click.js
@@ -1,9 +1,12 @@
 $(document).ready(function () {
   $(document).off("click", ".common-item-list").on("click", ".common-item-list", function() {
-    var win = window.open('//a.o333o.com/api/direct/278196?s1=&kw=', '_blank');
-    if (win) {
-        win.focus();
-    } else {
+    if (is_need_ads === true) {
+      var win = window.open('//a.o333o.com/api/direct/278196?s1=&kw=', '_blank');
+      if (win) {
+          win.focus();
+      } else {
+      }
     }
+
   });
 });
